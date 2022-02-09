c94f924357e9e7fc1dce0b6f7ce87d1b31d92bff
laurentbedu <bedulaurent@gmail.com>
2022-02-09 16:09:43 +0100

    ok

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,6 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import {Gender}  from "../models/gender.model";
 
 const HomeScreen = () => {
+
+  const [genders, setGenders] = useState([]);
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      const data = 
+        await (await fetch("http://localhost:5000/product/10")).text()
+      return data;
+      
+      //setGenders(genders);
+    };
+    fetchData().then(console.log).catch(console.error);
+  },[])
 
   return (
     <>
