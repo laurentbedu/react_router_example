aebfd5f77b48492a596aba426ba8878bf2b3bdf1
laurentbedu <bedulaurent@gmail.com>
2022-09-15 11:01:03 +0200

    ok

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react";
 
 const HomeScreen = () => {
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      const tests = 
+        await (await fetch("http://localhost:5000/test/1")).json()
+      console.log(tests);
+    }
+    fetchData().catch(console.error);
+
+  })
 
   return (
     <>
